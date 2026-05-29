05f44dc547ebfbd9b4f4c3b7ed2968aea4d225d9
shad0w <admin@dystopi.cc>
2026-05-29 11:15:38 -0700

    Release v1.27.26

--- js/sim.js
+++ js/sim.js
@@ -1,6 +1,6 @@
 /**
  * Browser-Sim Core Engine
- * Version: 1.27.25
+ * Version: 1.27.26
  */
 const _getSimStorage = () => (window.location.search.includes('chip') || window.self !== window.top) ? sessionStorage : localStorage;
 
